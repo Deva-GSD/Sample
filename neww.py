dc353bdc001ec7a6abff7632bb2f3c2cd22a7a27
deva <deva@gmail.com>
2023-09-24 22:30:23 +0530

    another sample

---
+++ neww.py
@@ -0,0 +1,12 @@
+a=26
+b=23
+print("HELLO EVERYONE")
+add=a+b
+sub=a-b
+multi=a*b
+div=a/b
+print("The addition of the value is:",add)
+print("The subtraction of the value is:",sub)
+print("The multiplication of the value is:",multi)
+print("The division of the value is:",div)
+print("This is sample one")
