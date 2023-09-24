44c8bf9be472991acfc5860ae75b5e37e14e9f87
deva <deva@gmail.com>
2023-09-24 23:10:27 +0530

    removed division

--- neww.py
+++ neww.py
@@ -5,9 +5,7 @@ print("HELLO EVERYONE")
 add=a+b
 sub=a-b
 multi=a*b
-div=a/b
 print("The addition of the value is:",add)
 print("The subtraction of the value is:",sub)
 print("The multiplication of the value is:",multi)
-print("The division of the value is:",div)
 print("This is sample one")
